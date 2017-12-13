82be598d6364b4303a9dff02a2653f5f01ae269b
Hikaru Suido <hikaru.suido@gmail.com>
2017-12-14 00:32:34 +0900

    This is the first commit. Add a README file and Create black screen in the frame.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rogue-like Game "Dungeon"
+
---
+++ RogueLikeGame.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+import java.awt.Color;
+// import javax.swing.JPanel;
+
+class RogueLikeGame {
+	public static void main(String[] args) {
+		JFrame fr = new JFrame("RogueLike");
+		
+		fr.setSize(400, 400);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.getContentPane().setBackground(new Color(0, 0, 0));
+
+		// RogueLikeGamePanel panel = new RogueLikeGamePanel();
+		// panel.setOpaque(false);
+		// fr.add(panel);
+		
+		fr.setVisible(true);
+	}
+}
+
+// class RogueLikeGamePanel extends JPanel {
+// 	RogueLikeGamePanel() {
+// 	}
+// 
+// 	@Override
+// 	public void paintComponent(Graphics g) {
+// 	}
+// }
+
